cf8914dcf308fa91c45728512762108ee951e70e
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-03-25 23:14:43 -0300

    Add smoth scroll to html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
   const locale = await getLocale();
 
   return (
-    <html lang={locale}>
+    <html lang={locale} className='scroll-smooth'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
