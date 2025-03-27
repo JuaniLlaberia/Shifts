b005012cdc3308e166f01ebea9add4ba0dad079b
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-03-27 11:31:45 -0300

    Add color to top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NextTopLoader showSpinner={false} />
+        <NextTopLoader showSpinner={false} color='#FFD230' />
         <Toaster />
 
         <ReactQueryProvider>
