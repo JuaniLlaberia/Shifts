5455c6a6e7665edbd923856348b62fa6c2331c64
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-03-29 22:31:23 -0300

    Create reusable components

---
+++ src/components/ui/input-wrapper.tsx
@@ -0,0 +1,32 @@
+import { CircleAlert } from 'lucide-react';
+import { type ReactNode } from 'react';
+
+import { Label } from './label';
+
+type InputWrapperProps = {
+  children: ReactNode;
+  inputId?: string;
+  label?: string;
+  error?: string;
+};
+
+const InputWrapper = ({
+  children,
+  inputId,
+  label,
+  error,
+}: InputWrapperProps) => {
+  return (
+    <div className='space-y-2'>
+      {label && <Label htmlFor={inputId}>{label}</Label>}
+      {children}
+      {error && (
+        <p className='flex items-center gap-2 text-sm text-destructive'>
+          <CircleAlert className='size-4' /> {error}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default InputWrapper;
