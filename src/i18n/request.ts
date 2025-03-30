c348eaf040c4e3e5aef2bad35204ddce4981c67e
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-03-29 22:30:36 -0300

    Add default locale for non-auth users

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,8 +4,13 @@ import { getAuthUser } from '@/access-data/user/get-auth-user';
 import { LOCALE_DICTIONARY } from '@/lib/dictionaries';
 
 export default getRequestConfig(async () => {
+  let locale: string;
   const user = await getAuthUser();
-  const locale = LOCALE_DICTIONARY[user.locale];
+  if (!user) {
+    locale = LOCALE_DICTIONARY['EN'];
+  } else {
+    locale = LOCALE_DICTIONARY[user.locale];
+  }
 
   return {
     locale,
