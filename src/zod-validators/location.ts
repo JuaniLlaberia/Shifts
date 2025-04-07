1a19defab95bc797ce7e1ec4e136315b75e3004f
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-04-07 20:16:40 -0300

    Add locations validation schemas

--- src/zod-validators/location.ts
+++ src/zod-validators/location.ts
@@ -9,3 +9,17 @@ export const createLocationValidator = z.object({
 
   isOnboarding: z.optional(z.boolean()),
 });
+
+export const updateLocationValidator = z.object({
+  businessId: z.string().cuid({ message: 'Invalid business id' }),
+  name: z.optional(z.string()),
+  address: z.string(),
+  latitude: z.number(),
+  longitude: z.number(),
+  locationId: z.string().cuid({ message: 'Invalid location id' }),
+});
+
+export const deleteLocationValidator = z.object({
+  businessId: z.string().cuid({ message: 'Invalid business id' }),
+  locationId: z.string().cuid({ message: 'Invalid location id' }),
+});
