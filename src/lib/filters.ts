f911622ff8326a5d6f66746a4308881a227c8154
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-04-07 20:16:30 -0300

    Create filters dictionaries

---
+++ src/lib/filters.ts
@@ -0,0 +1,12 @@
+export const EMPLOYEE_FILTERS = {
+  admin: {
+    label: 'Is Administrator',
+    field: 'admin',
+    options: ['true', 'false'],
+  },
+  status: {
+    label: 'Status',
+    field: 'status',
+    options: ['joined', 'pending'],
+  },
+};
