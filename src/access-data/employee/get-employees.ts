38ef48555847cdecb4cc24fbe270a6091c6e6c5e
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-04-08 19:13:37 -0300

    Update get-employees data function

--- src/access-data/employee/get-employees.ts
+++ src/access-data/employee/get-employees.ts
@@ -1,27 +1,44 @@
 'server only';
 
+import { EmployeeStatus, Prisma } from '@prisma/client';
+
 import { db } from '@/db';
 import { withEmployee } from '../auth-helper/employee-wrapper';
 
 type getEmployeesType = {
   businessId: string;
+  // Filters
+  admin?: string;
+  status?: EmployeeStatus;
   departmentId?: string;
+  // Pagination
   pageSize: number;
   page: number;
 } & { userId: string; employeeId: string; isAdmin: boolean };
 
 const getEmployeesBase = async ({
   businessId,
+  admin,
+  status,
   departmentId,
   isAdmin,
   page,
   pageSize,
 }: getEmployeesType) => {
   try {
+    const conditions: Prisma.EmployeeWhereInput[] = [{ businessId }];
+
+    if (status) conditions.push({ status });
+    if (admin)
+      conditions.push({
+        role: {
+          permissions: { equals: admin === 'true' ? 'ADMIN' : 'MEMBER' },
+        },
+      });
+    if (departmentId) conditions.push({ role: { departmentId } });
+
     const employees = await db.employee.findMany({
-      where: departmentId
-        ? { businessId, role: { departmentId } }
-        : { businessId },
+      where: { AND: conditions },
       select: {
         id: true,
         status: true,
