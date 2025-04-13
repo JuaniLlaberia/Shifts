30143aab803b830e3866b01ff60c1b348e4f81fb
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-04-13 16:03:18 -0300

    Update roles server function

--- src/access-data/role/get-roles.ts
+++ src/access-data/role/get-roles.ts
@@ -1,21 +1,35 @@
 'server only';
 
+import { Permissions, Prisma } from '@prisma/client';
+
 import { db } from '@/db';
 import { withEmployee } from '../auth-helper/employee-wrapper';
 
 type getRolesType = {
   businessId: string;
+  departmentId?: string;
+  permissions?: Permissions;
 } & { userId: string; employeeId: string; isAdmin: boolean };
 
 const getRolesBase = async ({
   businessId,
-
+  departmentId,
+  permissions,
   isAdmin,
 }: getRolesType) => {
   try {
+    const conditions: Prisma.RoleWhereInput[] = [{ businessId }];
+
+    if (departmentId) conditions.push({ departmentId });
+    if (permissions) conditions.push({ permissions });
+
     const roles = await db.role.findMany({
-      where: { businessId },
-      select: { id: true, name: true },
+      where: { AND: conditions },
+      include: {
+        department: {
+          select: { name: true },
+        },
+      },
     });
 
     return { roles, isAdmin };
